8c8dea3483be98c0b22cbfa4ea421ce41b0d3810
techtolentino <makertolentino@gmail.com>
2019-10-05 19:06:44 -0500

    first commit

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: `atxholidayhours`,
   shortName: `atxholidayhours`,
-  description: `Your primary resource for discovering what's open during the holidays.`,
+  description: `Your primary resource for discovering what's open in ATX during the holidays.`,
   url: 'https://atxholidayhours', // Domain of your site without prefix!
   prefix: '/',
   author: 'The Collective Legacy',
