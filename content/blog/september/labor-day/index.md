e7e74ed5410f3abd62947e0598ca0a1728f3571b
randytolentino <randytolentino@ibm.com>
2019-10-07 06:54:38 -0500

    add secion imgs

--- content/blog/september/labor-day/index.md
+++ content/blog/september/labor-day/index.md
@@ -4,3 +4,63 @@ date: "2015-04-01T22:12:03.284Z"
 thumbnail: ../ryan-barron-september.jpg
 description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
 ---
+
+## Major department stores
+
+##### Target
+
+Most Target stores will operate under normal hours
+
+##### Wal-Mart
+
+Most Wal-Mart stores will operate under normal hours
+
+##### Best Buy
+
+Most Best Buy stores will operate under normal hours
+
+##### Costco
+
+Most Costco stores will operate under normal hours
+
+##### Lowe's
+
+Most Lowe's stores will operate under normal hours
+
+##### Home Depot
+
+Most Home Depot stores will operate under normal hours
+
+##### Kohl's
+
+Most Kohl's stores will operate under normal hours
+
+##### Walgreens
+
+Most Walgreens stores will operate under normal hours
+
+##### CVS
+
+Most CVS stores will operate under normal hours
+
+## Grocery
+
+##### H-E-B
+
+Most H-E-B stores will operate under normal hours
+
+##### Whole Foods
+
+Most Whole Foods stores will operate under normal hours
+
+##### Sprouts
+
+Most Sprouts stores will operate under normal hours
+
+##### Randall's
+
+Most Randall's stores will operate under normal hours
+
+## Dining
+
+## Outdoors
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,15 @@ import { graphql, StaticQuery, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import PostCard from "../components/postCard"
+
+import Shopper1 from "../assets/images/shopper-1.png"
+import Shopper2 from "../assets/images/shopper-2.png"
+import Shopper3 from "../assets/images/shopper-3.png"
+import Shopper4 from "../assets/images/shopper-4.png"
+import Shopper5 from "../assets/images/shopper-5.png"
+import Shopper6 from "../assets/images/shopper-6.png"
+import Shopper7 from "../assets/images/shopper-7.png"
+import Shopper9 from "../assets/images/shopper-9.png"
 
 // import "../utils/global.scss"
 import "../utils/normalize.css"
@@ -18,7 +26,13 @@ const BlogIndex = ({ data }, location) => {
     <Layout title={siteTitle}>
       <SEO
         title="#ATXHolidayHours - Holiday Shopping Hours by Month"
-        keywords={[`atx`, `holidays`, `holiday shopping`, `shopping hours`, `what's open in atx`]}
+        keywords={[
+          `atx`,
+          `holidays`,
+          `holiday shopping`,
+          `shopping hours`,
+          `what's open in atx`,
+        ]}
       />
       {data.site.siteMetadata.description && (
         <header className="page-head">
@@ -27,42 +41,73 @@ const BlogIndex = ({ data }, location) => {
           </h2>
         </header>
       )}
-      
+
       <div className="post-feed">
         <section className="page-season-section page-season-fall">
           <div className="page-season">
             <h1 className="page-season-title">Autumn</h1>
             <div className="page-season-month">
               <h1>September</h1>
-              <Link className="page-season-link" to="/september/labor-day"><h3>Labor Day</h3></Link>
+              <Link className="page-season-link" to="/september/labor-day">
+                <h3>Labor Day</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>October</h1>
-              <Link className="page-season-link" to="/october/columbus-day"><h3>Columbus Day,</h3></Link>
-              <Link className="page-season-link" to="/october/halloween"><h3>Halloween</h3></Link>
+              <Link className="page-season-link" to="/october/columbus-day">
+                <h3>Columbus Day,</h3>
+              </Link>
+              <Link className="page-season-link" to="/october/halloween">
+                <h3>Halloween</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>November</h1>
-              <Link className="page-season-link" to="/october/veterans-day"><h3>Veteran's Day,</h3></Link>
-              <Link className="page-season-link" to="/october/thanksgiving"><h3>Thanksgiving</h3></Link>
+              <Link className="page-season-link" to="/october/veterans-day">
+                <h3>Veteran's Day,</h3>
+              </Link>
+              <Link className="page-season-link" to="/october/thanksgiving">
+                <h3>Thanksgiving</h3>
+              </Link>
+            </div>
+
+            <div className="page-season-shopper">
+              <img src={Shopper1} />
+              <img src={Shopper2} />
             </div>
           </div>
         </section>
         <section className="page-season-section page-season-winter">
+          <div className="page-season-shopper"></div>
           <div className="page-season">
             <h1 className="page-season-title">Winter</h1>
             <div className="page-season-month">
               <h1>December</h1>
-              <Link className="page-season-link" to="/october/christmas-holidays"><h3>Christmas Holidays</h3></Link>
+              <Link
+                className="page-season-link"
+                to="/october/christmas-holidays"
+              >
+                <h3>Christmas Holidays</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>January</h1>
-              <Link className="page-season-link" to="/october/new-years-day"><h3>New Year's Day,</h3></Link>
-              <Link className="page-season-link" to="/october/mlk-day"><h3>Martin Luther King Day</h3></Link>
+              <Link className="page-season-link" to="/october/new-years-day">
+                <h3>New Year's Day,</h3>
+              </Link>
+              <Link className="page-season-link" to="/october/mlk-day">
+                <h3>Martin Luther King Day</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>February</h1>
-              <Link className="page-season-link" to="/october/presidents-day"><h3>President's Day</h3></Link>
+              <Link className="page-season-link" to="/october/presidents-day">
+                <h3>President's Day</h3>
+              </Link>
+            </div>
+            <div className="page-season-shopper">
+              <img src={Shopper3} />
+              <img src={Shopper9} />
             </div>
           </div>
         </section>
@@ -71,15 +116,29 @@ const BlogIndex = ({ data }, location) => {
             <h1 className="page-season-title">Spring</h1>
             <div className="page-season-month">
               <h1>March</h1>
-              <Link className="page-season-link" to="/october/texas-independence-day"><h3>Texas Independence Day</h3></Link>
+              <Link
+                className="page-season-link"
+                to="/october/texas-independence-day"
+              >
+                <h3>Texas Independence Day</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>April</h1>
-              <Link className="page-season-link" to="/october/easter"><h3>Easter Sunday</h3></Link>
+              <Link className="page-season-link" to="/october/easter">
+                <h3>Easter Sunday</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>May</h1>
-              <Link className="page-season-link" to="/october/memorial"><h3>Memorial Day</h3></Link>
+              <Link className="page-season-link" to="/october/memorial">
+                <h3>Memorial Day</h3>
+              </Link>
+            </div>
+
+            <div className="page-season-shopper">
+              <img src={Shopper5} />
+              <img src={Shopper6} />
             </div>
           </div>
         </section>
@@ -92,11 +151,18 @@ const BlogIndex = ({ data }, location) => {
             </div>
             <div className="page-season-month">
               <h1>July</h1>
-              <Link className="page-season-link" to="/october/independence-day"><h3>Independence Day</h3></Link>
+              <Link className="page-season-link" to="/october/independence-day">
+                <h3>Independence Day</h3>
+              </Link>
             </div>
             <div className="page-season-month">
               <h1>August</h1>
               <h3 className="no-closures">(No holiday closures)</h3>
+            </div>
+
+            <div className="page-season-shopper">
+              <img src={Shopper4} />
+              <img src={Shopper7} />
             </div>
           </div>
         </section>
